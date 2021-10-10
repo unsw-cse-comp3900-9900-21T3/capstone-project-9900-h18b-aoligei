f38b9d3248f904a3ef6755ae5bc8d03968753523
Zoe Zhang <z5243683@ad.unsw.edu.au>
2021-10-11 00:14:42 +1100

    send email

--- User/forms.py
+++ User/forms.py
@@ -4,13 +4,26 @@ from User.models import User
 from django.contrib.auth.models import User
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField()
+    email = forms.CharField()
     password = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'password',
+        )
+
+class UserRegisterForm(forms.ModelForm):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = (
             'username',
             'password',
+            'email',
         )
 
--- User/models.py
+++ User/models.py
@@ -10,3 +10,14 @@ class User():
         verbose_name = 'user'
         verbose_name_plural = "users"
 
+class EmailVertifyCode(models.Model):
+    code = models.CharField(max_length=20,verbose_name="email vertify")
+    email = models.EmailField(max_length=200,verbose_name="vertify email")
+    send_type = models.IntegerField(choices=((1,'register'),(2,'forget'),(3,'change')),verbose_name="type of vertification")
+    # add_time = models.DateTimeField(default=datetime.now,verbose_name="add time")
+    def __str__(self):
+        return self.code
+    class Meta:
+        verbose_name = "Email verification code information"
+        verbose_name_plural = verbose_name
+
